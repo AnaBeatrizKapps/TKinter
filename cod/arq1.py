7658967de4a1165e95e6860b2733839483e49d3a
Ana Beatriz Kapps <50749914+AnaKapps@users.noreply.github.com>
2020-04-19 20:54:19 -0300

    Add files via upload

---
+++ cod/arq1.py
@@ -0,0 +1,10 @@
+class TesteReflection:
+    def test(self, age):
+        self._age = age
+    def car(self):
+        pass
+    @staticmethod
+    def House(): print("Life is short, you need Python")
+    @classmethod
+    def Project(cls, msg): return msg
+ 
---
+++ cod/arq2.py
@@ -0,0 +1,14 @@
+class test:
+    def Property(self,address): return address
+class test2:
+    def house(self):
+        pass
+class bank:
+    def deposity(self,account,value):
+        pass
+    @staticmethod
+    def Account(identity,name,value): 
+        print("value: {}").format(value)
+class example:
+    def Draft(self,name, description): 
+        pass
